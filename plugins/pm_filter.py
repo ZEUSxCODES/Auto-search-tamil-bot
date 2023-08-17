3f697bde5357333065501025f3cd57f17ca0071b
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2023-08-17 12:35:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1642,7 +1642,7 @@ async def auto_filter(client, msg, spoll=False):
                 if x in removes:
                     continue
                 else:
-                    search =in search + x + " "
+                    search = search + x + " "
             search = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|bro|bruh|broh|helo|that|find|dubbed|link|venum|iruka|pannunga|pannungga|anuppunga|anupunga|anuppungga|anupungga|film|undo|kitti|kitty|tharu|kittumo|kittum|movie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
             search = re.sub(r"\s+", " ", search).strip()
             search = search.replace("-", " ")
